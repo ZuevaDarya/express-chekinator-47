cbe41c237adc6c46d34d650df62b45886c3fac53
ZuevaDarya <80260736+ZuevaDarya@users.noreply.github.com>
2023-10-02 20:20:55 +0300

    Add files via upload

---
+++ express.js
@@ -0,0 +1,35 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/:DDMMYY', (req, res) => {
+  const DDMMYY = req.params.DDMMYY;
+  if (/^\d{6}$/.test(DDMMYY)) {
+    const dd = DDMMYY.slice(0, 2);
+    const mm = DDMMYY.slice(2, 4);
+    const yyyy = '20' + DDMMYY.slice(4);
+    const currentDate = dd + '-' + mm + '-' + yyyy;
+
+    const responseObject = {
+      date: currentDate,
+      login: 'zdarya', 
+    };
+
+    res.setHeader('Content-Type', 'application/json');
+    res.json(responseObject);
+  } else {
+    res.status(400).json({ error: 'Неверный формат DDMMYY' });
+  }
+});
+
+app.get('/api/rv/:abc', (req, res) => {
+  const abc = req.params.abc;
+  const reversedAbc = abc.split('').reverse().join('');
+  res.send(reversedAbc);
+});
+
+app.listen(1212, () => {
+  console.log('Server 1212 was started!');
+});
